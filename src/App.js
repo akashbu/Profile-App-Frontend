4bafc95549ddd291e3273fcc3af8c8efd9327ea8
Akash Butala <akash.butala.365@gmail.com>
2022-05-04 17:19:27 +0530

    Revert "My first commit"
    This reverts commit ae3bd6019098d3adbc8bccf6c1ada81e348b5e97.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import About from './container/About/About';
-import Footer from './container/Footer/Footer';
-import Header from './container/Header/Header';
-import Skills from './container/Skills/Skills';
-import Testimonials from './container/Testimonials/Testimonials';
-import Work from './container/Work/Work';
-import  Navbar from './components/Navbar/Navbar';
-import './App.scss';
+import logo from './logo.svg';
+import './App.css';
 
-
-const App = () => {
+function App() {
   return (
-    <div className="app">
-    <Navbar />
-    <Header />
-    <About />
-    <Work />
-    <Skills />
-    <Testimonials />
-    <Footer />
-  </div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
