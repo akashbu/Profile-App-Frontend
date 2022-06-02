cea517dd19dfd0027b742421fb4e5be13a57e3ef
Akash Butala <akash.butala.365@gmail.com>
2022-06-02 20:12:44 +0530

    Changed Readme file and Header file

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => (
         <div className="tag-cmp app__flex">
           <p className="p-text">BE in Information Technology </p>
           <p className="p-text">Software Engineer</p>
-          <p className="p-text">Full Stack Developer</p>
+          <p className="p-text">Full Stack Web Developer</p>
         </div>
       </div>
     </motion.div>
